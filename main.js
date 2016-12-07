9f834081f03a0a1c677413708487f9a17e73abc8
BrandoLucos <bc.lucas89@gmail.com>
2016-12-07 14:00:32 -0500

    Started a new landing page for myself

---
+++ main.js
@@ -0,0 +1,8 @@
+
+
+// $(document).ready(function() {
+//  $("little").onload(function(skills){
+//      for(var i = 0; i > skills.length; i++) {
+//      $("skills",[i]).fadein(slow);
+//      }
+//  })
