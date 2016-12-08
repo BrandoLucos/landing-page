7ef9bc30fe4b90873135c098c92320d19ed6c886
BrandoLucos <bc.lucas89@gmail.com>
2016-12-08 11:36:49 -0500

    For commits sake, lots of changes

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 
 
+
+var skills = ["HTML", "CSS", "JAVASCRIPT", "MONGODB", "MONGOOSE", "JQUERY", "RUBY", "RAILS"];
+
+window.onload = function(skills ) {
+  for(var i = 0; i < skills.length; i++) {
+    document.getElementById("little").style.write([i]);
+  }
+};
+
 // $(document).ready(function() {
 //  $("little").onload(function(skills){
 //      for(var i = 0; i > skills.length; i++) {
