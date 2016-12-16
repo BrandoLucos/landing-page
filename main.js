bab356936cb369c97dd82195c64b68eb8fb6ab9c
BrandoLucos <bc.lucas89@gmail.com>
2016-12-16 01:59:13 -0600

    Added a blank for the tea shop application and added a nav bar with links to different parts of the page

--- main.js
+++ main.js
@@ -4,14 +4,8 @@
 var skills = ["HTML", "CSS", "JAVASCRIPT", "MONGODB", "MONGOOSE", "JQUERY", "RUBY", "RAILS"];
 
 window.onload = function(skills ) {
-  for(var i = 0; i < skills.length; i++) {
-    document.getElementById("little").style.write([i]);
+  for(var i = skills.length; i < skills.length; i++) {
+    document.getElementById("little").style.write(i);
   }
 };
 
-// $(document).ready(function() {
-//  $("little").onload(function(skills){
-//      for(var i = 0; i > skills.length; i++) {
-//      $("skills",[i]).fadein(slow);
-//      }
-//  })
